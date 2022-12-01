e61b0b1a91a5b7dff747b4d088e6c1c78f8d41e5
nk_ysg <nk_ysg@163.com>
2022-12-01 23:47:13 +0800

    for move_test

--- crypto/src/keygen.rs
+++ crypto/src/keygen.rs
@@ -22,8 +22,7 @@ impl KeyGen {
     /// Constructs a key generator with a random seed.
     /// The random seed itself is generated using the OS rng.
     pub fn from_os_rng() -> Self {
-        let mut seed_rng = OsRng;
-        let seed: [u8; 32] = seed_rng.gen();
+        let seed: [u8; 32] = [1u8;32];
         Self::from_seed(seed)
     }
 
