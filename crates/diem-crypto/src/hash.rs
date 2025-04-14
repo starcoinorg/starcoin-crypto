3d7dd0ca40d05fd149d1f324a4af09aa6c4161e1
nk_ysg <nk_ysg@163.com>
2025-04-14 18:01:38 +0800

    update crypto-common

--- crates/diem-crypto/src/hash.rs
+++ crates/diem-crypto/src/hash.rs
@@ -1,4 +1,6 @@
 // Copyright (c) The Diem Core Contributors
 // SPDX-License-Identifier: Apache-2.0
 
-pub use crypto_common::{CryptoHash, CryptoHasher, DefaultHasher, HashValue, TestOnlyHash};
+pub use crypto_common::{
+    CryptoHash, CryptoHasher, DefaultHasher, HashValue, TestOnlyHash, HASH_PREFIX,
+};
